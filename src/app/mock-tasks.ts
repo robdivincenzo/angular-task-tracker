3216eb7d4b277e78ac1da720bf5e1c112efa6da9
Rob DiVincenzo <rob.divincenzo@gmail.com>
2022-10-24 23:43:40 -0400

    committing release

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,24 @@
+import { Task } from './Task';
+
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'May 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'May 6th at 1:30pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'May 8th at 5:30pm',
+        reminder: true,
+    },
+
+]
